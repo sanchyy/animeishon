40600d7b5da4d3d89e5da714f935413493b13a46
sanchy <vsanchez.gassull@gmail.com>
2019-05-02 20:05:24 +0200

    TODO: connections i multiplicacio de matriu 4D

--- hypercube/index.js
+++ hypercube/index.js
@@ -1,8 +1,16 @@
-let p4 = function (x,y,z,w) {this.x=x; this.y=y; this.z=z; this.w=w;}
-let p3 = function (x,y,z) {this.x=x; this.y=y; this.z=z;} 
+let p4  = function (x,y,z,w) {this.x=x; this.y=y; this.z=z; this.w=w;}
+let p3  = function (x,y,z) {this.x=x; this.y=y; this.z=z;} 
+
+let sin = (x) => { Math.sin(x) }; 
+let cos = (x) => { Math.cos(x) }; 
+let RM = (x)  => { [[1,0,0,0],[0,1,0,0],[0,0,cos(x), -sin(x)],[0,0,sin(x), cos(x)]] }
+
+const IM = [[1,0,0,0],[0,1,0,0],[0,0,1,0]];
+
 let hypercube  = []
 let projection = []
-const IM = [[1,0,0,0],[0,1,0,0],[0,0,1,0]];
+
+let t = 0;
 
 hypercube[0]  = new p4(-50,-50,-50,50);
 hypercube[1]  = new p4(50,-50,-50,50);
@@ -29,7 +37,7 @@ renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 //CAMERA
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera = new THREE.OrthographicCamera( window.innerWidth / -4, window.innerWidth / 4, window.innerHeight / 4, window.innerHeight / -4, 1, 1000 );
 camera.position.set(50,50,50);
 camera.lookAt(new THREE.Vector3(0,0,0));
 
@@ -40,6 +48,7 @@ controls.enableKeys = false;
 
 document.body.appendChild( renderer.domElement );
 
+//PROJECTION MATRIX
 function PM(mat,v) {
     let _x = mat[0][0]*v.x + mat[0][1]*v.y + mat[0][2]*v.z + mat[0][3]*v.w;
     let _y = mat[1][0]*v.x + mat[1][1]*v.y + mat[1][2]*v.z + mat[1][3]*v.w;
@@ -47,23 +56,27 @@ function PM(mat,v) {
     return new p3(_x,_y,_z);
 }
 
-
 function draw() {
     //SAVE PROJECTIONS
     let i = 0 
     for (let vertex of hypercube) { 
         hypercube[i] = PM(IM,vertex);
+        console.log(hypercube[i]);
         ++i;
     }
 
-    //DRAW LINES
-         
+    //DRAW CONNECTIONS 
+    for (let i = 0; i < hypercube.length; ++i) {
+        
+    }
     
 
 }
 
 function updateHypercube() {
     //UPDATE HYPERCUBE
+    
+    t += 0.2;
 }
 
 function animate() {
